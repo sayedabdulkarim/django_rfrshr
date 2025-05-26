9c5cd8c4fdb3ae15ccbf14c970e19bba67bb8582
Sayed Abdul <sakarim9124@gmail.com>
2025-05-26 22:21:27 +0530

    Add initial models for Student and Product; create Pipfile and Pipfile.lock

--- tutOne/core/home/models.py
+++ tutOne/core/home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    """
+    Model representing a student.
+    """
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    email = models.EmailField(unique=True)
+    address = models.TextField(blank=True, null=True)
+    image = models.ImageField()
+    file = models.FileField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Product(models.Model):
+    pass
