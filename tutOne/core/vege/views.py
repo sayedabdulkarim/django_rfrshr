bb6ca9551659ab7ec3a151c4ee49f9c6d00bf9ad
Sayed Abdul <sakarim9124@gmail.com>
2025-05-28 22:24:04 +0530

    Update templates and views for recipes: enhance layout with Bootstrap, add form fields, and include current year in footer

--- tutOne/core/vege/views.py
+++ tutOne/core/vege/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 # Create your views here.
 def receipes(request):
-    return render(request, 'receipes.html')
+    return render(request, 'receipes.html', { 'now': timezone.now() })
